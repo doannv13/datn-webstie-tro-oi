86e179932dae67ba1e925fbf6ab51544fef0ee15
Khánh Nguyên <nguyennkph27339@fpt.edu.vn>
2023-09-20 13:51:19 +0700

    fix setting

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -8,5 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     use HasFactory;
-    protected $guarded = [];
+    protected $fillable = [
+        'logo',
+        'address',
+        'support_phone',
+        'email',
+        'zalo'
+    ];
+    public $timestamps = true;
 }
