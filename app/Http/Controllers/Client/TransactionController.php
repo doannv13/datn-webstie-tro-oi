23c424fb4b4cbf7b1a1ff372e81998908675a5b6
thaole2003 <lmt.3102003@gmail.com>
2023-10-09 23:25:48 +0700

    add point and index point

---
+++ app/Http/Controllers/Client/TransactionController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $data= Transaction::with('user')->latest()->paginate(10);
+        return view('admin.transaction.index',compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        // dd($request->all());
+        $model = new Transaction();
+        $model->fill($request->all());
+        $model->save();
+        toastr()->success('Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi.','Đơn hàng sẽ được xác nhận sớm');
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/transaction/index.blade.php
@@ -0,0 +1,93 @@
+@extends('admin.layouts.master')
+@section('title', 'Danh sách đơn hàng cần duyệt')
+@section('content')
+
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="mt-0">Đơn hàng</h5>
+                <div class="table-responsive">
+                    <div class="mb-2 d-flex gap-1 ">
+                    </div>
+                    <table id="tech-companies-1" class="table table-centered mb-0 text-center">
+                        <thead>
+                        <tr>
+                            <th class="col-0.5">#</th>
+                            <th class="col-1.5">Người nạp</th>
+                            <th class="col-1.5">Số tiền</th>
+                            <th class="col-1.5">Phương thức</th>
+                            <th class="col-1.5">Ngày mua</th>
+                            <th class="col-1.5">Trạng thái</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($data as $key => $value)
+                            <tr id="row_@item.ID">
+                                <td class="tabledit-view-mode">{{ $key +1 }}</td>
+                                <td class="tabledit-view-mode">{!! substr($value->user->name, 0, 20) !!}</td>
+                                <td class="tabledit-view-mode">{!! substr($value->point, 0, 20) !!}</td>
+                                <td class="tabledit-view-mode">{!! substr($value->payment_method, 0, 20) !!}</td>
+                                <td class="tabledit-view-mode">{{ $value->created_at }}</td>
+                                <td class="tabledit-view-mode">{{ $value->status }}</td>
+                                {{-- <td>
+                                    <input data-id="{{ $value->id }}" class="toggle-class" type="checkbox"
+                                           data-onstyle="success" data-offstyle="danger" data-toggle="toggle"
+                                           data-onlabel="Bật" data-offlabel="Tắt"
+                                        {{ $value->status == 'active' ? 'checked' : '' }}>
+                                </td> --}}
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div> <!-- end .table-responsive-->
+            </div> <!-- end card-body -->
+        </div> <!-- end card -->
+    </div>
+@endsection
+
+
+@push('scripts')
+    <script>
+        new DataTable('#tech-companies-1');
+        $(function() {
+            $('.toggle-class').change(function() {
+                let status = $(this).prop('checked') == true ? 'active' : 'inactive';
+                let post_id = $(this).data('id');
+
+                $.ajax({
+                    type: "GET",
+                    dataType: "json",
+                    url: '{{ route('posts-status-change') }}',
+                    data: {
+                        'status': status,
+                        'post_id': post_id
+                    },
+                    success: function(data) {
+                        console.log(data);
+                        const Toast = Swal.mixin({
+                            toast: true,
+                            position: 'top-end',
+                            icon: 'success',
+                            showConfirmButton: false,
+                            timer: 3000
+                        })
+                        if ($.isEmptyObject(data.error)) {
+
+                            Toast.fire({
+                                icon: 'success',
+                                title: data.success,
+                            })
+
+                        } else {
+
+                            Toast.fire({
+                                icon: 'error',
+                                title: data.error,
+                            })
+                        }
+                    }
+                });
+            })
+        })
+    </script>
+@endpush
