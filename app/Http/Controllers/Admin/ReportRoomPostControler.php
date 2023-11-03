2fd044219b97f1a59a9a4e3f79d0980bbeee224c
Khánh Nguyên <nguyennkph27339@fpt.edu.vn>
2023-11-03 10:20:45 +0700

    update report

--- app/Http/Controllers/Admin/ReportRoomPostControler.php
+++ app/Http/Controllers/Admin/ReportRoomPostControler.php
@@ -7,6 +7,9 @@ use App\Models\RoomPost;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Exports\RoomPostExport;
+
 
 class ReportRoomPostControler extends Controller
 {
@@ -59,5 +62,26 @@ class ReportRoomPostControler extends Controller
         $room_post_default = RoomPost::query()->whereNull('service_id')->orWhere('time_end', '<', Carbon::now())->count();
         $total_room_post= RoomPost::select(DB::raw('DATE(created_at) as date'), DB::raw('COUNT(*) as total_posts'))->groupBy('date')->orderBy('date')->get();
         return view('admin.report.room-post.index', compact('date_start', 'date_end', 'room_post', 'room_post_today', 'room_post_accept', 'room_post_accept_today', 'room_post_pendding', 'room_post_pendding_today', 'room_post_vip', 'room_post_default','total_room_post'));
+    }
+
+    public function exportRoomPost() {
+        $roomPost = RoomPost::query()->count();
+        $roomPostToday = RoomPost::query()->whereDate('created_at', today())->count();
+        $roomPostToday = RoomPost::query()->where('status', 'accept')->count();
+        $roomPostAcceptToday = RoomPost::query()->where('status', 'accept')->whereDate('created_at', today())->count();
+        $roomPostPending = RoomPost::query()->where('status', 'pendding')->count();
+        $roomPostPendingToday = RoomPost::query()->where('status', 'pendding')->whereDate('created_at', today())->count();
+        $roomPostVIP = RoomPost::query()->whereNotNull('service_id')->where('time_end', '>', Carbon::now())->count();
+        $roomPostDefault = RoomPost::query()->whereNull('service_id')->orWhere('time_end', '<', Carbon::now())->count();
+
+        $totalRoomPost= RoomPost::select(DB::raw('DATE(created_at) as date'), DB::raw('COUNT(*) as total_posts'))->groupBy('date')->orderBy('date')->get();
+
+        return Excel::download(
+            new RoomPostExport(
+                $roomPost, $roomPostToday, $roomPostToday, $roomPostAcceptToday, $roomPostPending,
+                $roomPostPendingToday, $roomPostVIP, $roomPostDefault, $totalRoomPost
+            ),
+            'bao_cao_tin_dang.xlsx'
+        );
     }
 }
