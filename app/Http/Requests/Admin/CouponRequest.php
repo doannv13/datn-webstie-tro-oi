956c57eca23bb32253257831b17461457708fa08
Đoàn Nguyễn <nguyendoanjp12@gmail.com>
2023-11-21 18:12:00 +0700

    fix baiviet va coupon

--- app/Http/Requests/Admin/CouponRequest.php
+++ app/Http/Requests/Admin/CouponRequest.php
@@ -46,11 +46,11 @@ class CouponRequest extends FormRequest
 
             'type.required' => 'Kiểu không được để trống',
 
-            'value.required' => 'Mật khẩu phải bắt buộc nhập',
+            'value.required' => 'Giá trị bắt buộc nhập',
             'value.integer' => 'Giá trị phải là số',
 
 
-            'description.required' => 'Địa chỉ phải bắt buộc nhập',
+            'description.required' => 'Mô tả phải bắt buộc nhập',
 
             'status.required' => 'Trạng thái không được để trống',
 
